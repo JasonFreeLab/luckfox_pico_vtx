365972b8ecec823daa80c1155c5e404ab0a628e4
JasonFreeLab <1578989199@qq.com>
2024-11-05 19:33:25 +0800

    updata vi

--- src/luckfox_pico_rtsp_opencv/src/luckfox_mpi.cc
+++ src/luckfox_pico_rtsp_opencv/src/luckfox_mpi.cc
@@ -12,6 +12,8 @@
 
 #include "luckfox_mpi.h"
 
+#define BITRATE (2 * 1024)
+
 RK_U64 TEST_COMM_GetNowUs() {
 	struct timespec time = {0, 0};
 	clock_gettime(CLOCK_MONOTONIC, &time);
@@ -71,8 +73,7 @@ int vi_chn_init(int channelId, int width, int height) {
 	VI_CHN_ATTR_S vi_chn_attr;
 	memset(&vi_chn_attr, 0, sizeof(vi_chn_attr));
 	vi_chn_attr.stIspOpt.u32BufCount = buf_cnt;
-	vi_chn_attr.stIspOpt.enMemoryType =
-	    VI_V4L2_MEMORY_TYPE_DMABUF; // VI_V4L2_MEMORY_TYPE_MMAP;
+	vi_chn_attr.stIspOpt.enMemoryType = VI_V4L2_MEMORY_TYPE_DMABUF; // VI_V4L2_MEMORY_TYPE_MMAP;
 	vi_chn_attr.stSize.u32Width = width;
 	vi_chn_attr.stSize.u32Height = height;
 	vi_chn_attr.enPixelFormat = RK_FMT_YUV420SP;
@@ -88,62 +89,16 @@ int vi_chn_init(int channelId, int width, int height) {
 	return ret;
 }
 
-int vpss_init(int VpssChn, int width, int height) {
-	printf("%s\n",__func__);
-	int s32Ret;
-	VPSS_CHN_ATTR_S stVpssChnAttr;
-	VPSS_GRP_ATTR_S stGrpVpssAttr;
-
-	int s32Grp = 0;
-
-	stGrpVpssAttr.u32MaxW = 4096;
-	stGrpVpssAttr.u32MaxH = 4096;
-	stGrpVpssAttr.enPixelFormat = RK_FMT_YUV420SP;
-	stGrpVpssAttr.stFrameRate.s32SrcFrameRate = -1;
-	stGrpVpssAttr.stFrameRate.s32DstFrameRate = -1;
-	stGrpVpssAttr.enCompressMode = COMPRESS_MODE_NONE;
-
-	stVpssChnAttr.enChnMode = VPSS_CHN_MODE_USER;
-	stVpssChnAttr.enDynamicRange = DYNAMIC_RANGE_SDR8;
-	stVpssChnAttr.enPixelFormat = RK_FMT_RGB888;
-	stVpssChnAttr.stFrameRate.s32SrcFrameRate = -1;
-	stVpssChnAttr.stFrameRate.s32DstFrameRate = -1;
-	stVpssChnAttr.u32Width = width;
-	stVpssChnAttr.u32Height = height;
-	stVpssChnAttr.enCompressMode = COMPRESS_MODE_NONE;
-
-	s32Ret = RK_MPI_VPSS_CreateGrp(s32Grp, &stGrpVpssAttr);
-	if (s32Ret != RK_SUCCESS) {
-		return s32Ret;
-	}
-
-	s32Ret = RK_MPI_VPSS_SetChnAttr(s32Grp, VpssChn, &stVpssChnAttr);
-	if (s32Ret != RK_SUCCESS) {
-		return s32Ret;
-	}
-	s32Ret = RK_MPI_VPSS_EnableChn(s32Grp, VpssChn);
-	if (s32Ret != RK_SUCCESS) {
-		return s32Ret;
-	}
-
-	s32Ret = RK_MPI_VPSS_StartGrp(s32Grp);
-	if (s32Ret != RK_SUCCESS) {
-		return s32Ret;
-	}
-	return s32Ret;
-}
-
 int venc_init(int chnId, int width, int height, RK_CODEC_ID_E enType) {
 	printf("%s\n",__func__);
 	VENC_RECV_PIC_PARAM_S stRecvParam;
 	VENC_CHN_ATTR_S stAttr;
 	memset(&stAttr, 0, sizeof(VENC_CHN_ATTR_S));
 
-	// RTSP H264	
+	// RTSP H265
 	stAttr.stVencAttr.enType = enType;
-	//stAttr.stVencAttr.enPixelFormat = RK_FMT_YUV420SP;
-	stAttr.stVencAttr.enPixelFormat = RK_FMT_RGB888;	
-	stAttr.stVencAttr.u32Profile = H264E_PROFILE_MAIN;
+	stAttr.stVencAttr.enPixelFormat = RK_FMT_YUV420SP; //RK_FMT_RGB888
+	stAttr.stVencAttr.u32Profile = H265E_PROFILE_MAIN;
 	stAttr.stVencAttr.u32PicWidth = width;
 	stAttr.stVencAttr.u32PicHeight = height;
 	stAttr.stVencAttr.u32VirWidth = width;
@@ -152,9 +107,9 @@ int venc_init(int chnId, int width, int height, RK_CODEC_ID_E enType) {
 	stAttr.stVencAttr.u32BufSize = width * height * 3 / 2;
 	stAttr.stVencAttr.enMirror = MIRROR_NONE;
 		
-	stAttr.stRcAttr.enRcMode = VENC_RC_MODE_H264CBR;
-	stAttr.stRcAttr.stH264Cbr.u32BitRate = 3 * 1024;
-	stAttr.stRcAttr.stH264Cbr.u32Gop = 1;
+	stAttr.stRcAttr.enRcMode = VENC_RC_MODE_H265CBR;
+	stAttr.stRcAttr.stH265Cbr.u32BitRate = BITRATE;
+	stAttr.stRcAttr.stH265Cbr.u32Gop = 1;
 	RK_MPI_VENC_CreateChn(chnId, &stAttr);
 
 	memset(&stRecvParam, 0, sizeof(VENC_RECV_PIC_PARAM_S));
--- src/luckfox_pico_rtsp_opencv/src/main.cc
+++ src/luckfox_pico_rtsp_opencv/src/main.cc
@@ -28,35 +28,17 @@
 #include "rtsp_demo.h"
 #include "luckfox_mpi.h"
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#define VIDEO_WIGHT 1280
+#define VIDEO_HEIGHT 720
 
-#include "vtx_socket.h"
-
-int main(int argc, char *argv[]) {
-  system("RkLunch-stop.sh");
-  RK_S32 s32Ret = 0; 
-
-	int sX,sY,eX,eY;
-	int width    = 2304;
-    int height   = 1296;
-
-	char fps_text[16];
-	float fps = 0;
-	memset(fps_text,0,16);
-
-	//h264_frame	
-	VENC_STREAM_S stFrame;	
-	stFrame.pstPack = (VENC_PACK_S *)malloc(sizeof(VENC_PACK_S));
- 	VIDEO_FRAME_INFO_S h264_frame;
- 	VIDEO_FRAME_INFO_S stVpssFrame;
+int main(int argc, char *argv[])
+{
+	system("RkLunch-stop.sh");
 
 	// rkaiq init
-	RK_BOOL multi_sensor = RK_FALSE;	
+	RK_BOOL multi_sensor = RK_FALSE;
 	const char *iq_dir = "/etc/iqfiles";
 	rk_aiq_working_mode_t hdr_mode = RK_AIQ_WORKING_MODE_NORMAL;
-	//hdr_mode = RK_AIQ_WORKING_MODE_ISP_HDR2;
 	SAMPLE_COMM_ISP_Init(0, hdr_mode, multi_sensor, iq_dir);
 	SAMPLE_COMM_ISP_Run(0);
 
@@ -71,97 +53,72 @@ int main(int argc, char *argv[]) {
 	rtsp_session_handle g_rtsp_session;
 	g_rtsplive = create_rtsp_demo(554);
 	g_rtsp_session = rtsp_new_session(g_rtsplive, "/live/0");
-	rtsp_set_video(g_rtsp_session, RTSP_CODEC_ID_VIDEO_H264, NULL, 0);
+	rtsp_set_video(g_rtsp_session, RTSP_CODEC_ID_VIDEO_H265, NULL, 0);
 	rtsp_sync_video_ts(g_rtsp_session, rtsp_get_reltime(), rtsp_get_ntptime());
 
 	// vi init
 	vi_dev_init();
-	vi_chn_init(0, width, height);
+	vi_chn_init(0, VIDEO_WIGHT, VIDEO_HEIGHT);
 
-	// vpss init
-	vpss_init(0, width, height);
+	// venc init
+	RK_CODEC_ID_E enCodecType = RK_VIDEO_ID_HEVC;
+	venc_init(0, VIDEO_WIGHT, VIDEO_HEIGHT, enCodecType);
+	
+	// bind vi to venc
+	MPP_CHN_S stSrcChn, stvencChn;
 
-	// bind vi to vpss
-	MPP_CHN_S stSrcChn, stvpssChn;
 	stSrcChn.enModId = RK_ID_VI;
 	stSrcChn.s32DevId = 0;
 	stSrcChn.s32ChnId = 0;
 
-	stvpssChn.enModId = RK_ID_VPSS;
-	stvpssChn.s32DevId = 0;
-	stvpssChn.s32ChnId = 0;
-	printf("====RK_MPI_SYS_Bind vi0 to vpss0====\n");
-	s32Ret = RK_MPI_SYS_Bind(&stSrcChn, &stvpssChn);
+	stvencChn.enModId = RK_ID_VENC;
+	stvencChn.s32DevId = 0;
+	stvencChn.s32ChnId = 0;
+	printf("====RK_MPI_SYS_Bind vi0 to venc0====\n");
+	RK_S32 s32Ret = RK_MPI_SYS_Bind(&stSrcChn, &stvencChn);
 	if (s32Ret != RK_SUCCESS) {
 		RK_LOGE("bind 0 ch venc failed");
 		return -1;
 	}
 
-	// venc init
-	RK_CODEC_ID_E enCodecType = RK_VIDEO_ID_AVC;
-	venc_init(0, width, height, enCodecType);
+	float fps = 0;
 
-	vi_vtx_socket_init();
-	
+	//h265_frame	
+	VENC_STREAM_S stFrame;	
+	stFrame.pstPack = (VENC_PACK_S *)malloc(sizeof(VENC_PACK_S));
+
+	void *pData = RK_NULL;
+
 	while(1)
-	{	
-		
-
-		// get vpss frame
-		s32Ret = RK_MPI_VPSS_GetChnFrame(0,0, &stVpssFrame,-1);
-		if(s32Ret == RK_SUCCESS)
-		{
-			void *data = RK_MPI_MB_Handle2VirAddr(stVpssFrame.stVFrame.pMbBlk);
-
-			cv::Mat frame(height,width,CV_8UC3, data);	
-			sprintf(fps_text,"fps = %.2f",fps);		
-            cv::putText(frame,fps_text,
-							cv::Point(40, 40),
-							cv::FONT_HERSHEY_SIMPLEX,1,
-							cv::Scalar(0,255,0),2);
-			memcpy(data, frame.data, width * height * 3);					
-		}
-
-
-		// send stream
-		// encode H264	
-		RK_MPI_VENC_SendFrame(0, &stVpssFrame,-1);
+	{
 		// rtsp
 		s32Ret = RK_MPI_VENC_GetStream(0, &stFrame, -1);
 		if(s32Ret == RK_SUCCESS)
 		{
 			if(g_rtsplive && g_rtsp_session)
 			{
+				printf("fps = %.2f\n",fps);
 				//printf("len = %d PTS = %d \n",stFrame.pstPack->u32Len, stFrame.pstPack->u64PTS);	
-				void *pData = RK_MPI_MB_Handle2VirAddr(stFrame.pstPack->pMbBlk);
+				pData = RK_MPI_MB_Handle2VirAddr(stFrame.pstPack->pMbBlk);
 				rtsp_tx_video(g_rtsp_session, (uint8_t *)pData, stFrame.pstPack->u32Len,
 							  stFrame.pstPack->u64PTS);
 				rtsp_do_event(g_rtsplive);
 			}
 			RK_U64 nowUs = TEST_COMM_GetNowUs();
-			fps = (float) 1000000 / (float)(nowUs - stVpssFrame.stVFrame.u64PTS);			
+			fps = (float) 1000000 / (float)(nowUs - stFrame.pstPack->u64PTS);
 		}
 
 		// release frame 
-		s32Ret = RK_MPI_VPSS_ReleaseChnFrame(0, 0, &stVpssFrame);
-		if (s32Ret != RK_SUCCESS) {
-			RK_LOGE("RK_MPI_VI_ReleaseChnFrame fail %x", s32Ret);
-		}
 		s32Ret = RK_MPI_VENC_ReleaseStream(0, &stFrame);
 		if (s32Ret != RK_SUCCESS) {
 			RK_LOGE("RK_MPI_VENC_ReleaseStream fail %x", s32Ret);
 		}
 
 	}
-
-
-	RK_MPI_SYS_UnBind(&stSrcChn, &stvpssChn);
+	RK_MPI_SYS_UnBind(&stSrcChn, &stvencChn);
 	
 	RK_MPI_VI_DisableChn(0, 0);
 	RK_MPI_VI_DisableDev(0);
-	
-	RK_MPI_VPSS_StopGrp(0);
-	RK_MPI_VPSS_DestroyGrp(0);
 	
 	SAMPLE_COMM_ISP_Stop(0);
 
